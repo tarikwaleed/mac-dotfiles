ddf1df546914083085fa2a4184a19080f5b03f5a
tarikwaleed <support@otomatika.tech>
2026-07-28 20:19:54 +0300

    Add Ctrl+A/C/V as select-all/copy/paste, disabled while Terminal is frontmost
    Sends the underlying Cmd+<key> apps expect. Excluded from Terminal since
    shells use Ctrl+A (start of line, readline) and Ctrl+C (SIGINT) natively
    - losing those would break basic terminal use.

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -19,4 +19,26 @@ for key, appName in pairs(appHotkeys) do
   end)
 end
 
+-- Ctrl+A/C/V as select-all/copy/paste (Linux/Windows muscle memory), sent as the
+-- underlying Cmd+<key> the app actually expects. Disabled while Terminal is frontmost,
+-- since shells use Ctrl+A (start of line) and Ctrl+C (SIGINT) natively.
+local ctrlToCmdKeys = {"a", "c", "v"}
+local ctrlToCmdHotkeys = {}
+
+for _, key in ipairs(ctrlToCmdKeys) do
+  ctrlToCmdHotkeys[key] = hs.hotkey.bind({"ctrl"}, key, function()
+    hs.eventtap.keyStroke({"cmd"}, key)
+  end)
+end
+
+local terminalWatcher = hs.application.watcher.new(function(appName, eventType)
+  if appName == "Terminal" then
+    local action = (eventType == hs.application.watcher.activated) and "disable" or "enable"
+    for _, hotkey in pairs(ctrlToCmdHotkeys) do
+      hotkey[action](hotkey)
+    end
+  end
+end)
+terminalWatcher:start()
+
 hs.alert.show("Hammerspoon config loaded")
